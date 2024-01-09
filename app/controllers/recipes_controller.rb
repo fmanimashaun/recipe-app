64d45f477e830d975a8af5b5ee93cc73b3fa2d18
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2024-01-09 22:38:55 +0100

    Update the recipes controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,12 @@
 class RecipesController < ApplicationController
+  before_action :authenticate_user!, except: %i[public_index]
+  before_action :set_recipe, only: %i[show edit update destroy]
+
   def index
+  end
+
+  def public_index
+    @recipes = Recipe.includes(:user, :recipe_foods).where(public: true).order('created_at DESC')
   end
 
   def show
@@ -9,5 +16,17 @@ class RecipesController < ApplicationController
   end
 
   def edit
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_recipe
+    @recipe = Recipe.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def recipe_params
+    params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public, :user_id)
   end
 end
