b99ba45b91d16a061a837613db0119d33a0470bb
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2024-01-09 22:11:45 +0100

    Add basic styling to the authentication views

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -12,6 +12,9 @@ module.exports = {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
+      boxShadow: {
+        'black': '3px 3px 0 0 rgba(0, 0, 0, 1)',
+      },
     },
   },
   plugins: [
