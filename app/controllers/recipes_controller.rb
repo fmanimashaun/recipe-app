8738a327c847aac575ee41b9525100768cf55785
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2024-01-09 22:47:40 +0100

    Fix linter error

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,21 +2,17 @@ class RecipesController < ApplicationController
   before_action :authenticate_user!, except: %i[public_index]
   before_action :set_recipe, only: %i[show edit update destroy]
 
-  def index
-  end
+  def index; end
 
   def public_index
     @recipes = Recipe.includes(:user, :recipe_foods).where(public: true).order('created_at DESC')
   end
 
-  def show
-  end
+  def show; end
 
-  def new
-  end
+  def new; end
 
-  def edit
-  end
+  def edit; end
 
   private
 
