936286aac4b2b0707ce2d1022bb810072c845b57
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2024-01-09 22:38:40 +0100

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
- 
   devise_for :users
 
+  # the root of the application
+  root "recipes#public_index"
+
+  # redirect /recipes to the root of the application
+  get "/public_index", to: redirect("/")
+
+  # resources :recipes
   resources :recipes
-  root "recipes#index"
 end
