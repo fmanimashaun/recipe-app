04d211e4bf16c923ddd9504d3d3e6a9069f53f1f
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2024-01-12 16:51:37 +0100

    Update the food model

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,7 +1,26 @@
 class FoodsController < ApplicationController
-  def index
+  before_action :authenticate_user!
+  def index; end
+
+  def new
+    @food = Food.new
   end
 
-  def show new edit
+  def create
+    @food = Food.new(food_params)
+    @food.user = current_user
+
+    if @food.save
+      redirect_to @food, notice: 'Food was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  # Only allow a list of trusted parameters through.
+  def food_params
+    params.require(:food).permit(:name, :measurement_unit, :price, :quantity, :user_id)
   end
 end
