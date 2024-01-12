90dce122fa43fb074e236093aae4c6dacb4a9f3c
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2024-01-12 16:51:48 +0100

    Update views

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,10 @@
 Rails.application.routes.draw do
-  get 'foods/index'
-  get 'foods/show new edit'
   devise_for :users
 
-  # set root path for authenticated user
-  authenticated :user do
-    root 'recipes#index', as: :authenticated_root
-    get '/index', to: redirect('/')
-    get '/public_index', to: 'recipes#public_index' 
-  end
-
-  # set root path for unauthenticated user
-  unauthenticated do
-    root 'recipes#public_index', as: :unauthenticated_root
-    get '/public_index', to: redirect('/')
-  end
-
+  # define resources
   resources :recipes
+  resources :foods
+
+  # define root
+  root to: 'recipes#public_index'
 end
