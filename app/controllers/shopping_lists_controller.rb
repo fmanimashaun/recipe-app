be44c16ed1b8136010a1be7d841f38acbb3ce283
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2024-01-13 05:48:37 +0100

    Update views

--- app/controllers/shopping_lists_controller.rb
+++ app/controllers/shopping_lists_controller.rb
@@ -1,4 +1,4 @@
 class ShoppingListsController < ApplicationController
-  def show
+  def index
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-  get 'shopping_lists/show'
-  get 'recipe_foods/index'
   devise_for :users
 
   # define resources
   resources :recipes
   resources :foods
+  get 'general_shopping_list', to: 'shopping_lists#index'
 
   # define root
   root to: 'recipes#public_index'
