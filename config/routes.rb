e803f8fca3c17468743b66bd4506f056eb93db0a
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2024-01-13 17:57:06 +0100

    Update controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
+  get 'shopping_lists/index'
   devise_for :users
 
   # define resources
-  resources :recipes
   resources :foods
-  get 'general_shopping_list', to: 'shopping_lists#index'
+  resources :recipes do
+    resources :recipe_foods, only: %i[new create edit update destroy]
+  end
 
   # define root
   root to: 'recipes#public_index'
+  get '/general_shopping_list' , to: 'shopping_lists#index'
 end
--- spec/requests/shopping_lists_spec.rb
+++ spec/requests/shopping_lists_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe "ShoppingLists", type: :request do
-  describe "GET /show" do
+  describe "GET /index" do
     it "returns http success" do
-      get "/shopping_lists/show"
+      get "/shopping_lists/index"
       expect(response).to have_http_status(:success)
     end
   end
