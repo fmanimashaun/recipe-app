963515c8f3b74f459fa7ae2e37660c99f5dcb7eb
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2024-01-14 13:02:17 +0100

    Create the unit tests for the models

--- spec/models/recipe_food_spec.rb
+++ spec/models/recipe_food_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe RecipeFood, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { FactoryBot.create(:user) }
+  let(:food) { FactoryBot.create(:food, user: user) }
+  let(:recipe) { FactoryBot.create(:recipe, user: user) }
+  let(:recipe_food) { FactoryBot.build(:recipe_food, recipe: recipe, food: food) }
+
+  it 'is valid with valid attributes' do
+    expect(recipe_food).to be_valid
+  end
+
+  it 'is not valid without a quantity' do
+    recipe_food.quantity = nil
+    expect(recipe_food).not_to be_valid
+  end
+
+  it 'is not valid with a quantity less than or equal to 0' do
+    recipe_food.quantity = 0
+    expect(recipe_food).not_to be_valid
+  end
+
+  it 'is not valid without a recipe_id' do
+    recipe_food.recipe_id = nil
+    expect(recipe_food).not_to be_valid
+  end
+
+  it 'is not valid without a food_id' do
+    recipe_food.food_id = nil
+    expect(recipe_food).not_to be_valid
+  end
 end
--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -1,5 +1,55 @@
 require 'rails_helper'
 
 RSpec.describe Recipe, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { FactoryBot.create(:user) }
+  let(:recipe) { FactoryBot.build(:recipe, user: user) }
+
+  it 'is valid with valid attributes' do
+    expect(recipe).to be_valid
+  end
+
+  it 'is not valid without a name' do
+    recipe.name = nil
+    expect(recipe).not_to be_valid
+  end
+
+  it 'is not valid without a preparation_time' do
+    recipe.preparation_time = nil
+    expect(recipe).not_to be_valid
+  end
+
+  it 'is not valid with a preparation_time less than or equal to 0' do
+    recipe.preparation_time = 0
+    expect(recipe).not_to be_valid
+  end
+
+  it 'is not valid without a cooking_time' do
+    recipe.cooking_time = nil
+    expect(recipe).not_to be_valid
+  end
+
+  it 'is not valid with a cooking_time less than or equal to 0' do
+    recipe.cooking_time = 0
+    expect(recipe).not_to be_valid
+  end
+
+  it 'is not valid without a description' do
+    recipe.description = nil
+    expect(recipe).not_to be_valid
+  end
+
+  it 'is not valid without a public attribute' do
+    recipe.public = nil
+    expect(recipe).not_to be_valid
+  end
+
+  describe '#foods' do
+    it 'can have many foods' do
+      recipe.save
+      FactoryBot.create(:recipe_food, recipe: recipe)
+      FactoryBot.create(:recipe_food, recipe: recipe)
+
+      expect(recipe.foods.length).to eq 2
+    end
+  end
 end
